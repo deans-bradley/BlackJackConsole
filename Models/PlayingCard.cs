fbd019a5e23254569baa550ca8c0edeec7869858
Bradley Deans <bradleydeansdev@gmail.com>
2025-03-21 20:54:04 +0200

    Initial commit

---
+++ Models/PlayingCard.cs
@@ -0,0 +1,8 @@
+namespace BlackJackConsole.Models
+{
+    public class PlayingCards
+    {
+        public string Suit { get; set; } = String.Empty;
+        public string Value { get; set; } = String.Empty;
+    }
+}
